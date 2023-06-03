54e617220f70197af085c53cc39d1fb1c075260e
Arifnyacode <cybermen8@gmail.com>
2023-06-03 10:34:21 +0700

    mengganti nama administrator

--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -133,7 +133,7 @@
           <img src="<?= BASEURL; ?>/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">Administrator</a>
         </div>
       </div>
 
