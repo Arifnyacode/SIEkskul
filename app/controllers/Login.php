d29b3534fc64b34cf5373a0a0ccaca148fe8f578
Arifnyacode <cybermen8@gmail.com>
2023-06-10 11:14:39 +0700

    create function login

--- app/controllers/Login.php
+++ app/controllers/Login.php
@@ -2,8 +2,35 @@
 
 class Login extends Controller
 {
+    private $table = "users";
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
     public function index()
     {
         $this->view('login/index');
+    }
+
+
+    public function authentication(){
+        
+        if ($this->db==null) {
+            die("Koneksi ke database gagal: ");
+        }
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        // Query untuk memeriksa username dan password di database
+        $this->db->query("SELECT * FROM users WHERE email='$username' AND password='$password'");
+        $result=$this->db->single();
+        
+        if ($result->num_rows > 0) {
+            header("Location:http://localhost/SIEkskul/public/dashboard/index.php");
+        } else {
+            // Jika data tidak ditemukan, user gagal login
+            echo "Login gagal!";
+        } 
     }
 }
